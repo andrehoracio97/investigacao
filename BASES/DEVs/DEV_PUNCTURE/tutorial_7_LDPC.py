e5f47dd35a9cff12806aec78424ebffe344fe7ad
Andre <andresilvamail@gmail.com>
2020-03-17 11:06:51 +0000

    organizacao

---
+++ BASES/DEVs/DEV_PUNCTURE/tutorial_7_LDPC.py
@@ -0,0 +1,247 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: Tutorial
+# Author: Andre Silva
+# GNU Radio version: 3.7.13.5
+##################################################
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from PyQt4 import Qt
+from gnuradio import blocks
+from gnuradio import digital
+from gnuradio import eng_notation
+from gnuradio import fec
+from gnuradio import gr
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from optparse import OptionParser
+import pmt
+import puncture64_cpp
+import sys
+from gnuradio import qtgui
+
+
+class tutorial_7_LDPC(gr.top_block, Qt.QWidget):
+
+    def __init__(self, puncpat='11'):
+        gr.top_block.__init__(self, "Tutorial")
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Tutorial")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "tutorial_7_LDPC")
+        self.restoreGeometry(self.settings.value("geometry").toByteArray())
+
+
+        ##################################################
+        # Parameters
+        ##################################################
+        self.puncpat = puncpat
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.sps = sps = 4
+        self.nfilts = nfilts = 32
+        self.eb = eb = 0.22
+        self.H_dec = H_dec = fec.ldpc_H_matrix('/usr/local/share/gnuradio/fec/ldpc/n_1100_k_0442_gap_24.alist', 24)
+        self.H = H = fec.ldpc_H_matrix('/usr/local/share/gnuradio/fec/ldpc/n_1100_k_0442_gap_24.alist', 24)
+
+        self.tx_rrc_taps = tx_rrc_taps = firdes.root_raised_cosine(nfilts, nfilts, 1.0, eb, 11*sps*nfilts)
+
+        self.samp_rate = samp_rate = 3000000
+
+        self.rx_rrc_taps = rx_rrc_taps = firdes.root_raised_cosine(nfilts, nfilts*sps, 1.0, eb, 11*sps*nfilts)
+
+        self.punc_size = punc_size = 34
+        self.punc_replace = punc_replace = 127
+        self.punc_pattern = punc_pattern = 17179869182
+
+
+        self.pld_enc = pld_enc = map((lambda a: fec.ldpc_par_mtrx_encoder_make_H(H)), range(0,4))
+
+
+        self.pld_dec = pld_dec = map((lambda a: fec.ldpc_bit_flip_decoder.make(H_dec.get_base_sptr(), 10)), range(0,8))
+        self.pld_const = pld_const = digital.constellation_rect(([0.707+0.707j, -0.707+0.707j, -0.707-0.707j, 0.707-0.707j]), ([0, 1, 2, 3]), 4, 2, 2, 1, 1).base()
+        self.pld_const.gen_soft_dec_lut(8)
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.puncture64_cpp_puncture64_1 = puncture64_cpp.puncture64(punc_size, punc_pattern)
+        self.puncture64_cpp_depuncture64_0 = puncture64_cpp.depuncture64(punc_size, punc_pattern, 127)
+        self.blocks_repack_bits_bb_1_0_0_1 = blocks.repack_bits_bb(8, 1, '', False, gr.GR_MSB_FIRST)
+        self.blocks_file_source_0_0_1_0_0_1 = blocks.file_source(gr.sizeof_char*1, '/home/andre/Desktop/Files_To_Transmit/sequence55bytes_abc.txt', False)
+        self.blocks_file_source_0_0_1_0_0_1.set_begin_tag(pmt.PMT_NIL)
+        self.blocks_file_sink_2_0_0 = blocks.file_sink(gr.sizeof_char*1, '/home/andre/Desktop/Trasmited/seq_unpacked_depunc.txt', False)
+        self.blocks_file_sink_2_0_0.set_unbuffered(False)
+        self.blocks_file_sink_2_0 = blocks.file_sink(gr.sizeof_char*1, '/home/andre/Desktop/Trasmited/seq_unpacked_punc.txt', False)
+        self.blocks_file_sink_2_0.set_unbuffered(False)
+        self.blocks_file_sink_2 = blocks.file_sink(gr.sizeof_char*1, '/home/andre/Desktop/Trasmited/seq_unpacked.txt', False)
+        self.blocks_file_sink_2.set_unbuffered(False)
+
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.blocks_file_source_0_0_1_0_0_1, 0), (self.blocks_repack_bits_bb_1_0_0_1, 0))
+        self.connect((self.blocks_repack_bits_bb_1_0_0_1, 0), (self.blocks_file_sink_2, 0))
+        self.connect((self.blocks_repack_bits_bb_1_0_0_1, 0), (self.puncture64_cpp_puncture64_1, 0))
+        self.connect((self.puncture64_cpp_depuncture64_0, 0), (self.blocks_file_sink_2_0_0, 0))
+        self.connect((self.puncture64_cpp_puncture64_1, 0), (self.blocks_file_sink_2_0, 0))
+        self.connect((self.puncture64_cpp_puncture64_1, 0), (self.puncture64_cpp_depuncture64_0, 0))
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "tutorial_7_LDPC")
+        self.settings.setValue("geometry", self.saveGeometry())
+        event.accept()
+
+    def get_puncpat(self):
+        return self.puncpat
+
+    def set_puncpat(self, puncpat):
+        self.puncpat = puncpat
+
+    def get_sps(self):
+        return self.sps
+
+    def set_sps(self, sps):
+        self.sps = sps
+
+    def get_nfilts(self):
+        return self.nfilts
+
+    def set_nfilts(self, nfilts):
+        self.nfilts = nfilts
+
+    def get_eb(self):
+        return self.eb
+
+    def set_eb(self, eb):
+        self.eb = eb
+
+    def get_H_dec(self):
+        return self.H_dec
+
+    def set_H_dec(self, H_dec):
+        self.H_dec = H_dec
+
+    def get_H(self):
+        return self.H
+
+    def set_H(self, H):
+        self.H = H
+
+    def get_tx_rrc_taps(self):
+        return self.tx_rrc_taps
+
+    def set_tx_rrc_taps(self, tx_rrc_taps):
+        self.tx_rrc_taps = tx_rrc_taps
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+
+    def get_rx_rrc_taps(self):
+        return self.rx_rrc_taps
+
+    def set_rx_rrc_taps(self, rx_rrc_taps):
+        self.rx_rrc_taps = rx_rrc_taps
+
+    def get_punc_size(self):
+        return self.punc_size
+
+    def set_punc_size(self, punc_size):
+        self.punc_size = punc_size
+
+    def get_punc_replace(self):
+        return self.punc_replace
+
+    def set_punc_replace(self, punc_replace):
+        self.punc_replace = punc_replace
+
+    def get_punc_pattern(self):
+        return self.punc_pattern
+
+    def set_punc_pattern(self, punc_pattern):
+        self.punc_pattern = punc_pattern
+
+    def get_pld_enc(self):
+        return self.pld_enc
+
+    def set_pld_enc(self, pld_enc):
+        self.pld_enc = pld_enc
+
+    def get_pld_dec(self):
+        return self.pld_dec
+
+    def set_pld_dec(self, pld_dec):
+        self.pld_dec = pld_dec
+
+    def get_pld_const(self):
+        return self.pld_const
+
+    def set_pld_const(self, pld_const):
+        self.pld_const = pld_const
+
+
+def argument_parser():
+    parser = OptionParser(usage="%prog: [options]", option_class=eng_option)
+    parser.add_option(
+        "", "--puncpat", dest="puncpat", type="string", default='11',
+        help="Set puncpat [default=%default]")
+    return parser
+
+
+def main(top_block_cls=tutorial_7_LDPC, options=None):
+    if options is None:
+        options, _ = argument_parser().parse_args()
+
+    from distutils.version import StrictVersion
+    if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls(puncpat=options.puncpat)
+    tb.start()
+    tb.show()
+
+    def quitting():
+        tb.stop()
+        tb.wait()
+    qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
+    qapp.exec_()
+
+
+if __name__ == '__main__':
+    main()
